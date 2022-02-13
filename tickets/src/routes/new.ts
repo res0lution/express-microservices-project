863e394c5d1ab024c07406089200f16b6a2b17ef
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2022-02-13 16:38:41 +0300

    add nats

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,14 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import nats from 'node-nats-streaming';
 import { requireAuth, validateRequest } from '@sgtickets/common';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
-
-const stan = nats.connect('ticketing', 'tickets', {
-  url: 'http://nats-srv:4222',
-});
 
 router.post(
   '/api/tickets',
@@ -29,14 +24,6 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
-
-    const event = {
-      type: 'ticket:created',
-      data: ticket,
-    };
-    stan.publish('ticket:created', JSON.stringify(event), () => {
-      console.log('Ticket creation event published');
-    });
 
     res.status(201).send(ticket);
   }
